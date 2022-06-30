be7b222b460e0e4ffdc5a49291767ad0f4a1ed45
Brett Brewer <brett.brewer@vividseats.com>
2022-06-29 21:24:11 -0500

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+    // 'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'react': {
+      'version': 'detect', // React version. "detect" automatically picks the version you have installed.
+    },
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': ['error'],
+
+  },
+};
